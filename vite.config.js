68fb0dc012ad5bbef267ee53397220d7379c7b9a
yapusss <haarisnursalim@gmail.com>
2025-10-30 21:39:58 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Set base path for GitHub Pages. Adjust if your repo name differs.
   base: "/haaris-point-of-sale/",
   plugins: [
     react({
