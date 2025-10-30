bd583a41451476cc9d82d0f9a632e2988b592f0e
yapusss <haarisnursalim@gmail.com>
2025-10-30 21:37:29 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set base path for GitHub Pages. Adjust if your repo name differs.
+  base: "/haaris-point-of-sale/",
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+});
